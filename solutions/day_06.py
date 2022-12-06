39cdf191132674af9298d7a6893fddbca9b242aa
linknmike <mikeheidal@gmail.com>
2022-12-05 22:22:43 -0800

    Changed name of variable to not shadow builtin

--- solutions/day_06.py
+++ solutions/day_06.py
@@ -1,9 +1,9 @@
 
 def find_index_of_unique_character_set(threshold: int) -> int:
     with open(f"../inputs/day_06.txt", 'r') as input_file:
-        input = input_file.read()
-        for i in range(threshold, len(input), 1):
-            if len(set(input[i-threshold:i])) == threshold:
+        transmission = input_file.read()
+        for i in range(threshold, len(transmission), 1):
+            if len(set(transmission[i-threshold:i])) == threshold:
                 return i
     return -1
 
