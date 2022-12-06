2aaf275c4d49b71388a9714396aceabba9eb1d46
linknmike <mikeheidal@gmail.com>
2022-12-05 21:09:09 -0800

    Day 6

--- solutions/day_06.py
+++ solutions/day_06.py
@@ -1,14 +1,25 @@
 
+def find_index_of_unique_character_set(threshold: int) -> int:
+    with open(f"../inputs/day_06.txt", 'r') as input_file:
+        buf_index = 0
+        transmission_index = 0
+        buf = [-1] * threshold
+        input = input_file.read()
+        while len(set(buf)) < threshold or -1 in buf:
+            buf[buf_index] = input[transmission_index]
+            transmission_index += 1
+            buf_index = ((buf_index + 1) % threshold)
+        return transmission_index
+
+
 def part_1():
-    with open(f"../inputs/day_06.txt", 'r') as input_file:
-        pass
+    print(f"Part 1: {find_index_of_unique_character_set(4)}")
         
-        
+
 def part_2():
-    with open(f"../inputs/day_06.txt", 'r') as input_file:
-        pass
-        
-        
+    print(f"Part 2: {find_index_of_unique_character_set(14)}")
+
+
 if __name__ == '__main__':
     part_1()
     part_2()
