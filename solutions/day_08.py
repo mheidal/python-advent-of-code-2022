8070c6822aca978b91ff5076a48d5fd2a49e0667
linknmike <mikeheidal@gmail.com>
2022-12-07 21:53:11 -0800

    Day 8 refactoring

--- solutions/day_08.py
+++ solutions/day_08.py
@@ -1,10 +1,14 @@
-
-def get_scenic_score():
-    scores = []
+def get_grid():
     grid = []
     with open(f"../inputs/day_08.txt", 'r') as input_file:
         for line in input_file.readlines():
             grid.append([int(x) for x in list(line.strip())])
+    return grid
+
+
+def get_scenic_score():
+    scores = []
+    grid = get_grid()
     for row_index, row in enumerate(grid):
         for col_index, tree in enumerate(row):
             if row_index == 0 or col_index == 0 or col_index == len(grid) - 1 or row_index == len(grid) - 1:
@@ -12,9 +16,9 @@ def get_scenic_score():
             column = [grid[r][col_index] for r in range(len(grid))]
             directions = [
                 reversed(column[0:row_index]),  # North/Up
-                column[row_index+1:],  # South/Down
+                column[row_index + 1:],  # South/Down
                 reversed(row[0:col_index]),  # West/Left
-                row[col_index+1:],  # East/Right
+                row[col_index + 1:],  # East/Right
             ]
             score = 1
             for direction in directions:
@@ -27,12 +31,10 @@ def get_scenic_score():
             scores.append(score)
     return max(scores)
 
-def do():
+
+def get_num_visible_trees():
     count = 0
-    grid = []
-    with open(f"../inputs/day_08.txt", 'r') as input_file:
-        for line in input_file.readlines():
-            grid.append([int(x) for x in list(line.strip())])
+    grid = get_grid()
     for row_index, row in enumerate(grid):
         for col_index, tree in enumerate(row):
             column = [grid[r][col_index] for r in range(len(grid))]
@@ -47,24 +49,24 @@ def do():
                 max(column[row_index + 1:] or [1000]) < tree,
             ]):
                 if not any([
-                row_index == 0,
-                col_index == 0,
-                row_index == len(grid) - 1,
-                col_index == len(row) - 1,]):
+                    row_index == 0,
+                    col_index == 0,
+                    row_index == len(grid) - 1,
+                    col_index == len(row) - 1,
+                ]):
                     print(f"({row_index}, {col_index}), tree={tree}")
                 count += 1
     return count
 
 
 def part_1():
-    x = do()
-    print(x)
+    print(get_num_visible_trees())
 
 
 def part_2():
     print(get_scenic_score())
-        
-        
+
+
 if __name__ == '__main__':
     # part_1()
     part_2()
